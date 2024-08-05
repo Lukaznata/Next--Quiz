564c613c03a157e2f2f241e46a894635c98cd5a2
Lucas Nathan Tonin <lucas18tonin@gmail.com>
2024-08-05 12:48:58 -0300

    alterado o texto do menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Button from "@/components/Button";
 export default function Home() {
   return (
     <div className={styles.home}>
-      <h1>Clique para começar o Quiz!</h1>
+      <h1>Clique para começar o Quiz! Boa sorte!</h1>
       <Button text="COMEÇAR" href="/start" />
     </div>
   );
